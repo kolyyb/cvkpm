1bd62fddd17d494f7633f2d3feab5449b9eec799
kolyyb <karine73@gmail.com>
2025-07-26 20:50:57 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kolyyb.github.io',
+  base: 'cvkpm',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
